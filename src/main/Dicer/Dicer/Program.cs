9a45bc56d13c3a62c04530d7203fc1e72cb1afd1
Beefcapone <beefcapone@gmail.com>
2017-03-03 09:35:06 +0100

    Random d6 roll
    I have made a loop that allows to roll a new dice and exits on escape..

--- src/main/Dicer/Dicer/Program.cs
+++ src/main/Dicer/Dicer/Program.cs
@@ -10,6 +10,20 @@ namespace Dicer
     {
         static void Main(string[] args)
         {
+            Console.Clear();
+            Random rand = new Random();
+            Console.WriteLine("Tryk enter for at rulle terning, Tryk esc for at exit!");
+            while (true)
+            {                
+                ConsoleKeyInfo keyPress = Console.ReadKey();
+                if (keyPress.Key == ConsoleKey.Enter)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Tryk enter for at rulle terning, Tryk esc for at exit!");
+                    Console.WriteLine(rand.Next(5) + 1);                    
+                }
+                else if (keyPress.Key == ConsoleKey.Escape) break;
+            }
         }
     }
 }
