2314fb687619ec5c6eeac39997399ec81514d21d
Beefcapone <beefcapone@gmail.com>
2017-03-03 10:22:44 +0100

    Menu added
    Menu with choice of multiple dice...

--- src/main/Dicer/Dicer/Program.cs
+++ src/main/Dicer/Dicer/Program.cs
@@ -8,21 +8,61 @@ namespace Dicer
 {
     class Program
     {
+        static int numDice = 1;
+        static Boolean running = true;
+        static string def1 = "Press Enter to roll, M for Menu and Esc for exit!";
+
         static void Main(string[] args)
         {
-            Console.Clear();
             Random rand = new Random();
-            Console.WriteLine("Tryk enter for at rulle terning, Tryk esc for at exit!");
-            while (true)
+            
+            Console.Clear();
+            Console.WriteLine(def1);
+
+            while (running)
             {                
                 ConsoleKeyInfo keyPress = Console.ReadKey();
                 if (keyPress.Key == ConsoleKey.Enter)
                 {
                     Console.Clear();
-                    Console.WriteLine("Tryk enter for at rulle terning, Tryk esc for at exit!");
-                    Console.WriteLine(rand.Next(5) + 1);                    
+                    Console.WriteLine(def1);
+                    for (int i = 0; i < numDice; ++i)
+                    {
+                        Console.WriteLine("Terning " + (i + 1) + " " + (rand.Next(5) + 1));
+                    }
                 }
-                else if (keyPress.Key == ConsoleKey.Escape) break;
+                else if (keyPress.Key == ConsoleKey.M) Menu();
+                else if (keyPress.Key == ConsoleKey.Escape || keyPress.Key == ConsoleKey.Q) running = false;
+            }
+        }
+
+        static void Menu()
+        {
+            while (running)
+            {
+                Console.Clear();
+                Console.WriteLine("Press backspace to go back or esc to exit..");
+               
+                Console.WriteLine("Number of dice: " + numDice);
+
+                ConsoleKeyInfo keyPress = Console.ReadKey();
+                if (keyPress.Key == ConsoleKey.RightArrow)
+                {
+                    ++numDice;
+                }
+                else if (keyPress.Key == ConsoleKey.LeftArrow)
+                {
+                    --numDice;
+                    numDice = Math.Max(numDice, 1);
+                    // if (numDice < 1) numDice = 1;                   
+                }
+                else if (keyPress.Key == ConsoleKey.Backspace)
+                {
+                    Console.Clear();
+                    Console.WriteLine(def1);
+                    break;
+                }
+                else if (keyPress.Key == ConsoleKey.Escape || keyPress.Key == ConsoleKey.Q) running = false;
             }
         }
     }
